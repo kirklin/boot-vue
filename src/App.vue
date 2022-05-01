b5a36f4aec2d2e3c4106d8a88bd0fa81ad2eebcb
Kirk Lin <linkirk@163.com>
2022-05-01 17:24:31 +0800

    refactor: Footer components

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div class="font-chinese antialiased">
     <Navbar />
     <RouterView />
+    <Footer />
   </div>
 </template>
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 </script>
