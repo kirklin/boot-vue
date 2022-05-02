3f447584a835db62a1508b4fd93894d969404ae6
Kirk Lin <linkirk@163.com>
2022-05-02 23:45:11 +0800

    feat: add log tool

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,6 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
+import { logLabeled } from "./utils/logger";
+logLabeled("I'm ready!  ⸜(๑'ᵕ'๑)⸝⋆*", "log", "", "color: #66ccff");
 </script>
