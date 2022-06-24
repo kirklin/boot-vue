27ef487d8d9f65d116e1ba337d601c02e343c993
Kirk Lin <linkirk@163.com>
2022-06-24 11:18:23 +0800

    chore: add error pages

---
+++ src/views/errorPages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col items-center gap-20 bg-base-100 py-20">
+    <div class="card w-96 bg-base-100">
+      <div class="card-body">
+        <h2 class="card-title">Error {{ errorCode }}!</h2>
+        <p>We're sorry, something is not right, page not found!</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary" @click="back">Back !</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const errorCode = ref<number>(404);
+const back = () => {
+  router.push({ name: "Home" });
+};
+</script>
