29db6cf977c6d3485b72c3e95eebfe38584e5b30
Kirk Lin <linkirk@163.com>
2022-08-31 17:23:11 +0800

    fix: eslint config

--- src/views/home/components/Hero.vue
+++ src/views/home/components/Hero.vue
@@ -12,7 +12,7 @@ const testStore = () => {
 
 <template>
   <div class="hero mt-[-4rem] min-h-screen text-primary-content">
-    <!--  <div class="hero min-h-screen from-primary to-secondary" style="background-image: url(https://img.js.design/assets/Resources/background/home-bg-2.png);">-->
+    <!--  <div class="hero min-h-screen from-primary to-secondary" style="background-image: url(https://img.js.design/assets/Resources/background/home-bg-2.png);"> -->
     <div class="hero-overlay place-items-center bg-gradient-to-bl from-primary to-base-100"></div>
     <div class="hero-content text-center">
       <div class="max-w-md">
