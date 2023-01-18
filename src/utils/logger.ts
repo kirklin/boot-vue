6df09748e7ff68972bff2e30845d5d8d3127f91c
Kirk Lin <linkirk@163.com>
2023-01-19 00:37:24 +0800

    chore: logger use env info

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -62,7 +62,7 @@ function _log(
     argsToPass.push(args);
   }
 
-  const editorLabelText = "ViteBoot";
+  const editorLabelText = import.meta.env.VITE_GLOB_APP_TITLE;
   const editorLabelStyle = `line-height: 1em;
             color: #006FEA;
             display: inline-block;
