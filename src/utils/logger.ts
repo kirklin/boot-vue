47026f4a4a78b1f41ec2d1a58e2ea36941d4c310
Kirk Lin <linkirk@163.com>
2023-01-22 03:20:01 +0800

    fix(lint): typescript-eslint/restrict-template-expressions

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -62,7 +62,7 @@ function _log(
     argsToPass.push(args);
   }
 
-  const editorLabelText = import.meta.env.VITE_GLOB_APP_TITLE;
+  const editorLabelText = String(import.meta.env.VITE_GLOB_APP_TITLE);
   const editorLabelStyle = `line-height: 1em;
             color: #006FEA;
             display: inline-block;
