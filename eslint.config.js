1bc142dd91f3497a2cf6043c1c08de55789d6de6
Kirk Lin <linkirk@163.com>
2025-04-18 18:18:58 +0800

    style: add eslint-plugin-vuejs-accessibility

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,9 @@ import kirklin from "@kirklin/eslint-config";
 export default kirklin({
   unocss: true,
   formatters: true,
+  typescript: true,
   pnpm: true,
+  vue: {
+    a11y: true,
+  },
 });
